b98c57e21c92f2a0ad31387bb1f3b909832525fa
dmacias7982 <dmacias7982@gmail.com>
2025-04-04 14:40:14 -0500

    Add files via upload

---
+++ # hello.py
@@ -0,0 +1,9 @@
+name = input("What´s your name: ")
+
+# hello.py
+def greet(name):
+    print(f"Hello, {name}! Welcome to my GitHub repo 😊")
+
+# Example usage
+if __name__ == "__main__":
+    greet(name)
